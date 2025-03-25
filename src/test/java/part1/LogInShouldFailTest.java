c13c10f7ecd07a99b1fc7396aaabfa6eaf3c283e
Kiran Setty <68366418+kiran-src@users.noreply.github.com>
2025-03-25 14:06:33 +0200

    Init

---
+++ src/test/java/part1/LogInShouldFailTest.java
@@ -0,0 +1,53 @@
+package part1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class LogInShouldFailTest {
+
+  WebDriver driver;
+
+  @BeforeClass
+  public void setUp() throws MalformedURLException {
+//    driver = new ChromeDriver();
+//    driver.manage().window().maximize();
+
+    ChromeOptions options = new ChromeOptions();
+    options.setCapability("browserName", "chrome");
+    options.setCapability("platformName", "LINUX");
+    driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+  }
+
+  @AfterClass
+  public void tearDown() {
+    driver.quit();
+  }
+
+  @Test
+  public void testLoggingIntoApplication() throws InterruptedException {
+    Thread.sleep(2000);
+    WebElement username = driver.findElement(By.name("username"));
+    username.sendKeys("Admin");
+
+    var password = driver.findElement(By.name("password"));
+    password.sendKeys("admin123");
+
+    driver.findElement(By.tagName("button")).click();
+    Thread.sleep(2000);
+    String actualResult = driver.findElement(By.tagName("h6")).getText();
+    String expectedResult = "Dashboard";
+    Assert.assertNotEquals(actualResult, expectedResult);
+  }
+}
